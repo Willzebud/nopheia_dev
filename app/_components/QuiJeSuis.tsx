a09a309b1a58ca0c9a211697f8ab5e41474919d1
willyam ribiere <willyamrib@gmail.com>
2025-04-19 15:25:25 +0200

    Sous section 1.7

--- app/_components/QuiJeSuis.tsx
+++ app/_components/QuiJeSuis.tsx
@@ -6,14 +6,14 @@ import { Section } from "./Section";
 export const QuiJeSuis = () => {
   return (
     <Section>
-      <div className="relative z-0 w-full mx-auto overflow-hidden xl:w-[80%] max-w-[95%] min-h-[600px] flex flex-col lg:flex-row items-center justify-between">
+      <div className="relative z-0 w-full mx-auto overflow-hidden xl:w-[80%] max-w-[95%] min-h-[600px] flex flex-col lg:flex-row items-center justify-between border border-8 border-dotted border-brand-dark rounded-xl">
         {/* Colonne texte */}
         <div className="z-10 w-full">
-          <h2 className="text-2xl md:text-3xl lg:text-4xl text-center text-brand-primary mb-12">
+          <h2 className="text-2xl md:text-3xl py-4 lg:text-4xl text-center text-brand-primary mb-4 lg:mb-12">
             Nopheïa
           </h2>
-          <div className="flex flex-col lg:flex-row items-center justify-center lg:gap-16 gap-4 w-full max-w-6xl mx-auto">
-            <div className="flex-shrink-0 md:mb-0 mx-auto md:mx-0">
+          <div className="flex flex-col lg:flex-row items-center justify-center lg:gap-10 gap-4 w-full max-w-6xl mx-auto">
+            <div className="flex-shrink-0 md:mb-0 mx-auto md:mx-0 pl-6">
               <div
                 className="relative w-40 h-40 md:w-48 md:h-48 lg:w-56 lg:h-56 rounded-full overflow-hidden shadow-lg"
                 style={{
@@ -29,7 +29,7 @@ export const QuiJeSuis = () => {
                 />
               </div>
             </div>
-            <p className="text-base sm:text-xl text-justify text-brand-dark mt-4">
+            <p className="text-base sm:text-xl text-justify text-brand-dark mt-4 px-4 py-4 lg:pr-8">
               Nopheïa est une marque fondée par Ophélie, ostéopathe diplômée
               d&rsquo;État et ambassadrice de la méthode GAD® depuis 3 ans.{" "}
               <br />
--- app/page.tsx
+++ app/page.tsx
@@ -36,6 +36,10 @@ export default function Home() {
       <NosSoins />
 
       <QuiJeSuis />
+
+      <Spacing />
+
+      <Spacing />
     </main>
   );
 }
