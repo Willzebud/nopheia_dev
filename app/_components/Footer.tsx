8208a445a1512e6d9f06bf1d27ae0b2ed83a9bc6
willyam ribiere <willyamrib@gmail.com>
2025-04-22 22:05:54 +0200

    Qu'est ce que le GAD  3.1

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -5,8 +5,10 @@ import { Vague } from "./icons/Vague";
 export const Footer = () => {
   return (
     <div className="relative z-0 w-full mx-auto overflow-hidden xl:w-[100%] max-w-[100%] min-h-[50px]">
-      <Vague />
-      <div className="relative bg-brand-dark" />
+      <div className="h-[200px]">
+        <Vague className="w-full h-full" />
+      </div>
+      <div className="relative bg-brand-dark h-[100px]" />
     </div>
   );
 };
