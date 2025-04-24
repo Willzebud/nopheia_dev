0c30a07a8a664b2ea810235d02c54ff59f36c4d5
willyam ribiere <willyamrib@gmail.com>
2025-04-24 23:30:33 +0200

    Resultat 1.1.3

--- app/resultats/page.tsx
+++ app/resultats/page.tsx
@@ -1,6 +1,5 @@
 import { Footer } from "../_components/Footer";
 import { Header } from "../_components/Header";
-import { Spacing } from "../Spacing";
 import { Introduction } from "./Introduction";
 
 export default function ResultatsPage() {
